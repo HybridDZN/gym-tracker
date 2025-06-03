5356bacee1f22c23be3b607e6b8376acabd4ba95
xero <marius.scutea.00@gmail.com>
2025-06-03 20:30:00 +0930

    Removed old SvelteKit files and fixed .prettierrc

--- src/lib/components/Button.svelte
+++
@@ -1,5 +0,0 @@
-<script>
-	import { Button } from '$lib/components/ui/button/index.js';
-</script>
-
-<Button variant="outline">Add entry</Button>
--- src/lib/components/Form.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import { Label } from '$lib/components/ui/label/index.js';
-	import Dropdown from '$lib/components/Dropdown.svelte';
-	let data = ['Flat Chest Press', 'Incline Chest Press', 'Jelq'];
-</script>
-
-<Label for="exercise" class="">Your current sets</Label>
-
-<Dropdown {data} />
