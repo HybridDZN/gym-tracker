2f97b9fcc31878c2e188302038522a37e708aa51
Marius Scutea <marius.scutea.00@gmail.com>
2025-06-27 18:30:34 +0930

    fix: lowered image res

--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -162,7 +162,7 @@ export function AuthPage() {
       </div>
       <div className="relative hidden bg-muted lg:block">
         <img
-          src="\images\background.jpg"
+          src="\images\background.png"
           alt="Image"
           className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
         />
